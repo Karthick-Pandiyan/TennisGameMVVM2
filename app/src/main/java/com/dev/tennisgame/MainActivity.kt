73fe8cdcc1b1411c3aea29af34a5f6247cf664ab
016 <>
2020-01-21 23:25:32 +0530

    Consumed the solution and implemented it in UI to play the game.

--- app/src/main/java/com/dev/tennisgame/MainActivity.kt
+++ app/src/main/java/com/dev/tennisgame/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.dev.tennisgame
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tennisGame: TennisGame
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val playerOne = "John"
+        val playerTwo = "Harry"
+        tennisGame = TennisGame(playerOne, playerTwo)
+
+        updateScore()
+        btnPlayerOne.setOnClickListener {
+            tennisGame.addScoreToPlayerOne()
+            updateScore()
+        }
+
+        btnPlayerTwo.setOnClickListener {
+            tennisGame.addScoreToPlayerTwo()
+            updateScore()
+        }
+
+        tennisGame.winner.observe(this, Observer {
+            btnPlayerOne.isEnabled = false
+            btnPlayerTwo.isEnabled = false
+        })
+    }
+
+    private fun updateScore(){
+        txtScoreView.text = tennisGame.getScore()
     }
 }
